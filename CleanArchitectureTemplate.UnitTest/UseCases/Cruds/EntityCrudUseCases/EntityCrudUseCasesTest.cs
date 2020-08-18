c789711952618809b230b15680ebfab037686d37
Darian Benito <darian.benito@gmail.com>
2020-08-18 14:36:31 -0300

    Init UnitTest project.-

---
+++ CleanArchitectureTemplate.UnitTest/UseCases/Cruds/EntityCrudUseCases/EntityCrudUseCasesTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CleanArchitectureTemplate.UnitTest.UseCases.Cruds.EntityCrudUseCases
+{
+    public class EntityCrudUseCasesTest
+    {
+        [Fact]
+        public void CreateEntityUseCase_ShouldReturnCreatedEntityId()
+        {
+
+        }
+    }
+}
